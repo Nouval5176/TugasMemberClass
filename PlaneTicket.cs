034b0e0928a96893e313ee18a0105938855dab02
Nouval5176 <127763913+Nouval5176@users.noreply.github.com>
2023-04-06 14:51:29 +0700

    Add files via upload

---
+++ PlaneTicket.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class PlaneTicket
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int Cost{ get; set; }
+        public string Currency { get; set; }
+        public string convert { get; set; }
+        public int convertcost { get; set; }
+
+        public void PrintSummary()
+        {
+            string convert = "";
+            if (Currency =="USD")
+            {
+                convert = "IDR";
+            }
+            else if (Currency == "IDR")
+            {
+                convert = "USD";
+            }
+
+            int convertcost = 0;
+            if (Currency == "USD")
+            {
+                convertcost = Cost * 15000;
+            }
+            else if (Currency == "IDR")
+            {
+                convertcost = Cost/15000;
+            }
+
+            Console.WriteLine("Origin: {0}", Origin);
+            Console.WriteLine("Destination: {0}", Destination);
+            Console.WriteLine("Cost: {0} ({1})", Cost,Currency);
+            Console.WriteLine("Convert to {0}: {1}\n", convert,convertcost);
+        }
+        
+   
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTicket ticket1 = new PlaneTicket();
+            PlaneTicket ticket2 = new PlaneTicket();
+
+            ticket1.Origin ="JOGJA" ;
+            ticket1.Destination = "PKU";
+            ticket1.Cost = 100;
+            ticket1.Currency = "USD";
+            ticket1.PrintSummary();
+
+            ticket2.Origin = "JOGJA";
+            ticket2.Destination = "PKU";
+            ticket2.Cost = 75000;
+            ticket2.Currency = "IDR";
+            ticket2.PrintSummary();
+
+
+            Console.ReadKey();
+        }
+    }
+}
